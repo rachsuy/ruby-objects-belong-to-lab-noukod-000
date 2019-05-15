80d44d4d36fc288ba1aabd69f21a3c5c9e4474bb
Rachele Suy <rachelesuy@gmail.com>
2019-05-15 15:14:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,10 @@
-8class artist 
-  attr_accessor :name
-  
-  
- def initialize(name)
-   @name = name
-end
+
+class Artist
+  attr_accessor :name, :genre
+ 
+  def initialize(name, genre) 
+    @name = name
+    @genre = genre
+  end
+ 
 end
